917bee12c57e29ae638061f7877a332a6830c61f
Lianke <1026193951@sjtu.edu.cn>
2018-08-28 13:56:11 +0800

    画出所有conversation audio的V/A 波动曲线

--- analysis.py
+++ analysis.py
@@ -5,7 +5,6 @@ import matplotlib.pyplot as plt
 #talk : 2-13
 talk_length = [52,22,9,11,30,45,4,19,10,32,43,19]
 
-user_id = [i for i in range(60)]
 data = pd.read_csv('data/test_data_pure.csv')
 
 
@@ -18,85 +17,63 @@ text_valence_error = [{} for j in range(12)]
 
 cols = ['UserID','Arousal']
 
-def array2Dic(array):
-    dic = {}
-    for i in array:
-        dic[i[0]] = i[1]
-    return dic
-
-def cal_error_abs(a,b):
-    result = 0
-    if(len(a) == len(b) and len(a) > 0):
-        for (key,item) in a.items():
-            result += np.abs(item - b[key])
-        return float(result / len(a))
-    else:
-        return 0
-
-#  此段为画出每句text/fumoji 与audio的误差折线图
-# for i in range(2,14):
-#     for j in range(1,talk_length[i-2]+1):
-#         text_dic = array2Dic(text_data[text_data['Talk'] == i][text_data['Sentence'] == j][cols].values)
-#         audio_dic = array2Dic(audio_data[audio_data['Talk'] == i][audio_data['Sentence'] == j][cols].values)
-#         fumoji_dic = array2Dic(fumoji_data[fumoji_data['Talk'] == i][fumoji_data['Sentence'] == j][cols].values)
-#         text_error = cal_error_abs(text_dic,audio_dic)
-#         fumoji_error = cal_error_abs(fumoji_dic,audio_dic)
-#         text_valence_error[i-2][j] = text_error
-#         fumoji_valence_error[i-2][j] = fumoji_error
-
-# for i in range(12):
-#     print('Talk {} text {} error mean : {}'.format(i+2,cols, np.abs(text_valence_error[i]).mean()))
-#     print('Talk {} fumoji {} error mean : {}'.format(i+2,cols, np.abs(fumoji_valence_error[i]).mean()))
-#     plt.figure()
-#     plt.title('Talk {} {} error comparison'.format(i+2,cols))
-#     plt.plot(np.abs(text_valence_error[i]),label='text error')
-#     plt.plot(np.abs(fumoji_valence_error[i]),label = 'fumoji error')
-#     plt.xlabel('sentence')
-#     plt.legend()
-#     plt.show()
-
-
+# def array2Dic(array):
+#     dic = {}
+#     for i in array:
+#         dic[i[0]] = i[1]
+#     return dic
+#
+# def cal_error_abs(a,b):
+#     result = 0
+#     if(len(a) == len(b) and len(a) > 0):
+#         for (key,item) in a.items():
+#             result += np.abs(item - b[key])
+#         return float(result / len(a))
+#     else:
+#         return 0
 
 # 以第10段对话为例，画出一段话中V A各自升降趋势图
+
+user_id = [[] for i in range(12)]
+
+for i in range(2,14):#每段话有哪些user做了
+    user_id[i-2] = list(set((audio_data[audio_data['Talk'] == i]['UserID'].values)))
+
+talk_id = [2,3,4,5,6,7,8,9,10,11,12,13]#conversation的列表
 
 def generate_2D_array(length):
     return [[] for i in range(length)]
 
-
-def plot_figure(array,name,type):
+def plot_figure(array,name,type,index):
     plt.figure()
     plt.title('{} {} trace'.format(name,type))
     for i in range(len(array)):
-        plt.plot(array[i],label = 'userid {}'.format(user_id[i]))
+        plt.plot(array[i],label = 'userid {}'.format(user_id[index][i]))
     plt.legend()
     plt.show()
 
-user_id_10 = [20,22,27,28,29]
-talk_id = 10
-valence_audio = generate_2D_array(len(user_id))
-arousal_audio = generate_2D_array(len(user_id))
-valence_text = generate_2D_array(len(user_id))
-arousal_text = generate_2D_array(len(user_id))
-valence_fumoji = generate_2D_array(len(user_id))
-arousal_fumoji = generate_2D_array(len(user_id))
+#user_id_10 = [20,22,27,28,29]
+#user_id = [19,20,21,23,24,27,28,29,30,31,32,33]
 
-count = 0
-for i in user_id:
-    valence_audio[count] = audio_data[audio_data['Talk'] == talk_id][audio_data['UserID'] == i]['Valence'].values
-    arousal_audio[count] = audio_data[audio_data['Talk'] == talk_id][audio_data['UserID'] == i]['Arousal'].values
-    valence_text[count] = text_data[text_data['Talk'] == talk_id][text_data['UserID'] == i]['Valence'].values
-    arousal_text[count] = text_data[text_data['Talk'] == talk_id][text_data['UserID'] == i]['Arousal'].values
-    valence_fumoji[count] = fumoji_data[fumoji_data['Talk'] == talk_id][fumoji_data['UserID'] == i]['Valence'].values
-    arousal_fumoji[count] = fumoji_data[fumoji_data['Talk'] == talk_id][fumoji_data['UserID'] == i]['Arousal'].values
-    count += 1
 
-# plot_figure(valence_audio,'audio','valence')
-# plot_figure(arousal_audio,'audio','arousal')
+valence_audio_all = []
+arousal_audio_all = []
 
-# plot_figure(valence_fumoji,'fumoji','valence')
-# plot_figure(arousal_fumoji,'fumoji','arousal')
-# plot_figure(valence_text,'text','valence')
-# plot_figure(arousal_text,'text','arousal')
+for talk in talk_id:
+    count = 0
+    valence_audio = generate_2D_array(len(user_id[talk-2]))
+    arousal_audio = generate_2D_array(len(user_id[talk-2]))
+    for i in user_id[talk - 2]:
+        valence_audio[count] = audio_data[audio_data['Talk'] == talk][audio_data['UserID'] == i]['Valence'].values
+        arousal_audio[count] = audio_data[audio_data['Talk'] == talk][audio_data['UserID'] == i]['Arousal'].values
+        count += 1
+    valence_audio_all.append(valence_audio)
+    arousal_audio_all.append(arousal_audio)
+
+for i in range(12):
+    plot_figure(valence_audio_all[i],'audio','valence',i)
+    plot_figure(arousal_audio_all[i],'audio','arousal',i)
+
 
 
 
