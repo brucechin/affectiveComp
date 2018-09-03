518b1f14a8ab9253cb390e613f3fa3d95aaa520b
Lianke <1026193951@sjtu.edu.cn>
2018-09-03 23:31:28 +0800

    按annotation获取对应的VA 数组

--- analysis_by_quadrant.py
+++ analysis_by_quadrant.py
@@ -18,32 +18,63 @@ arousal_text_error  = [[] for i in range(4)]
 valence_fumoji_error = [[] for i in range(4)]
 arousal_fumoji_error = [[] for i in range(4)]
 
+users_of_talk = []
+for i in range(2, 14):  # 每段话有哪些user做了
+    users_of_talk.append(list(set((audio_data[audio_data['Talk'] == i]['UserID'].values))))
 
 for talk in talk_id:
-    text_valence_error_tmp = []
-    text_arousal_error_tmp = []
-    for sentence in range(1,talk_length[talk-2] + 1):
-        audio_valence_mean = np.mean(audio_data[audio_data['Talk'] == talk][audio_data['Sentence'] == sentence][['Valence']].values)
-        audio_arousal_mean = np.mean(audio_data[audio_data['Talk'] == talk][audio_data['Sentence'] == sentence][['Arousal']].values)
-        text_valence_mean = np.mean(text_data[text_data['Talk'] == talk][text_data['Sentence'] == sentence]['Valence'].values)
-        text_arousal_mean = np.mean(text_data[text_data['Talk'] == talk][text_data['Sentence'] == sentence]['Arousal'].values)
-        fumoji_valence_mean = np.mean(fumoji_data[fumoji_data['Talk'] == talk][fumoji_data['Sentence'] == sentence]['Valence'].values)
-        fumoji_arousal_mean = np.mean(fumoji_data[fumoji_data['Talk'] == talk][fumoji_data['Sentence'] == sentence]['Arousal'].values)
-        loc = 0 #判断audio 情感在第几象限
-        if(audio_arousal_mean > 0 and audio_valence_mean > 0):
-            loc = 0
-        elif(audio_valence_mean < 0 and audio_arousal_mean > 0):
-            loc = 1
-        elif(audio_valence_mean < 0 and audio_arousal_mean < 0):
-            loc = 2
-        elif(audio_valence_mean > 0 and audio_arousal_mean < 0):
-            loc = 3
-        valence_text_error[loc].append(np.abs(text_valence_mean - audio_valence_mean))
-        arousal_text_error[loc].append(np.abs(text_arousal_mean - audio_arousal_mean))
-        valence_fumoji_error[loc].append(np.abs(fumoji_valence_mean - audio_valence_mean))
-        arousal_fumoji_error[loc].append(np.abs(fumoji_arousal_mean - audio_arousal_mean))
+    if talk != 6:
+        for sentence in range(1,talk_length[talk-2] + 1):
+            text_valence_error_tmp = []
+            text_arousal_error_tmp = []
+            fumoji_valence_error_tmp = []
+            fumoji_arousal_error_tmp = []
+            for user in users_of_talk[talk-2]:
+                text_data_tmp = text_data[text_data['Talk'] == talk][text_data['Sentence'] == sentence][text_data['UserID'] == user]
+                fumoji_data_tmp = fumoji_data[fumoji_data['Talk'] == talk][fumoji_data['Sentence'] == sentence][fumoji_data['UserID'] == user]
+                audio_data_tmp = audio_data[audio_data['Talk'] == talk][audio_data['Sentence'] == sentence][audio_data['UserID'] == user]
+                if(len(text_data_tmp) > 0 and len(fumoji_data_tmp) > 0 and len(audio_data_tmp) > 0):
+                    text_valence_error_tmp.append(text_data_tmp['Valence'].values[0] - audio_data_tmp['Valence'].values[0])
+                    text_arousal_error_tmp.append(text_data_tmp['Arousal'].values[0] - audio_data_tmp['Arousal'].values[0])
+                    fumoji_valence_error_tmp.append(fumoji_data_tmp['Valence'].values[0] - audio_data_tmp['Valence'].values[0])
+                    fumoji_arousal_error_tmp.append(fumoji_data_tmp['Arousal'].values[0] - audio_data_tmp['Arousal'].values[0])
+
+
+
+            audio_valence_mean = np.mean(audio_data[audio_data['Talk'] == talk][audio_data['Sentence'] == sentence][['Valence']].values)
+            audio_arousal_mean = np.mean(audio_data[audio_data['Talk'] == talk][audio_data['Sentence'] == sentence][['Arousal']].values)
+            text_valence_mean = np.mean(text_data[text_data['Talk'] == talk][text_data['Sentence'] == sentence]['Valence'].values)
+            text_arousal_mean = np.mean(text_data[text_data['Talk'] == talk][text_data['Sentence'] == sentence]['Arousal'].values)
+            fumoji_valence_mean = np.mean(fumoji_data[fumoji_data['Talk'] == talk][fumoji_data['Sentence'] == sentence]['Valence'].values)
+            fumoji_arousal_mean = np.mean(fumoji_data[fumoji_data['Talk'] == talk][fumoji_data['Sentence'] == sentence]['Arousal'].values)
+            loc = 0 #判断audio 情感在第几象限
+            if(audio_arousal_mean > 0 and audio_valence_mean > 0):
+                loc = 0
+            elif(audio_valence_mean < 0 and audio_arousal_mean > 0):
+                loc = 1
+            elif(audio_valence_mean < 0 and audio_arousal_mean < 0):
+                loc = 2
+            elif(audio_valence_mean > 0 and audio_arousal_mean < 0):
+                loc = 3
+            # print('talk {} sentence {}'.format(talk,sentence))
+            # print('text valence error std{}'.format(np.std(text_valence_error_tmp)))
+            # print('text arousal error std{}'.format(np.std(text_arousal_error_tmp)))
+            # print('fumoji valence error std{}'.format(np.std(fumoji_valence_error_tmp)))
+            # print('fumoji_arousal_error std{}'.format(np.std(fumoji_arousal_error_tmp)))
+
+            bottleneck = 0.8
+            if(np.std(text_valence_error_tmp) < bottleneck):
+                valence_text_error[loc].append(np.abs(text_valence_mean - audio_valence_mean))
+            if(np.std(text_arousal_error_tmp) < bottleneck):
+                arousal_text_error[loc].append(np.abs(text_arousal_mean - audio_arousal_mean))
+            if(np.std(fumoji_valence_error_tmp) < bottleneck):
+                valence_fumoji_error[loc].append(np.abs(fumoji_valence_mean - audio_valence_mean))
+            if(np.std(fumoji_arousal_error_tmp) < bottleneck):
+                arousal_fumoji_error[loc].append(np.abs(fumoji_arousal_mean - audio_arousal_mean))
+
+print('when remove error std lager than {}'.format(bottleneck))
 
 for i in range(4):
-    #print('Fumoji the {} quarter valence error mean {} arousal error mean {}, has {} sentences'.format(i+1,np.mean(valence_fumoji_error[i]),np.mean(arousal_fumoji_error[i]),len(valence_text_error[i])))
+    print('Fumoji the {} quarter valence error mean {} arousal error mean {}, has {} sentences'.format(i+1,np.mean(valence_fumoji_error[i]),np.mean(arousal_fumoji_error[i]),len(valence_text_error[i])))
     print('Text the {} quarter valence error mean {} arousal error mean {}, has {} sentences'.format(i+1,np.mean(valence_text_error[i]),np.mean(arousal_text_error[i]),len(valence_text_error[i])))
 
